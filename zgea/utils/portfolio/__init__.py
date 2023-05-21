cd0b6154a26ceeb417ce48d892f8e47b95de64d2
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2023-05-21 14:27:09 +0200

    Added drawdown portfolio for varying cash reserve.

--- zgea/utils/portfolio/__init__.py
+++ zgea/utils/portfolio/__init__.py
@@ -1,6 +1,7 @@
 from .portfolio import Portfolio
 from .ma_portfolio import MAPortfolio
 from .vol_portfolio import VolPortfolio
+from .drawdown_portfolio import DrawdownPortfolio
 from .asset import Asset
 from .tax_model import TaxModel
 from .null_tax_model import NullTaxModel
